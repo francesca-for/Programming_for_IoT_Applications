9427a262b2ec431e33801d58008c9adb3bd2bf29
Francesca Fornasier <francesca.fornasier@icloud.com>
2024-11-28 13:53:42 +0100

    iot labs and exercises

---
+++ labs/lab02/lab02es1.py
@@ -0,0 +1,14 @@
+import cherrypy
+import json
+
+class WebCalculator:
+    exposed = True
+    def GET(sefl, *uri, **param):
+        ...
+        self.add(operation, operands)
+    
+    def add(self, operation, operands):
+        ...
+
+if __name__ == "main":
+
